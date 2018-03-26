6d817f52dcababa6f81850b955f89bd88111da8b
eliascourt <elias@k1nd.ltd>
2018-03-26 16:31:36 +0100

    Slightly, and I mean slightly, improved set spec...

--- bones/src/test/scala/ltd/k1nd/pets/bones/syntax/SetOpsSpec.scala
+++ bones/src/test/scala/ltd/k1nd/pets/bones/syntax/SetOpsSpec.scala
@@ -1,5 +1,6 @@
 package ltd.k1nd.pets.bones.syntax
 
+import org.scalacheck.Gen
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -8,9 +9,18 @@ class SetOpsSpec
     with GeneratorDrivenPropertyChecks
     with Matchers {
 
-  "simple test" should "pass" in {
-    val s = Set(1, 2, 3)
-    s.doesNotContain(4) should equal(true)
+  val setGenerator = Gen.nonEmptyContainerOf[Set, Int](Gen.chooseNum(1, 100))
+
+  "A set" should "return false for not containing its head element" in {
+    forAll(setGenerator){ set =>
+      val elemInSet = set.head
+      set.doesNotContain(elemInSet) should be(false)
+    }
   }
 
+  "A set of positive numbers" should "return true for not containing a negative number" in {
+    forAll(setGenerator){ set =>
+      set.doesNotContain(-1) should be(true)
+    }
+  }
 }
