8b6cdbfe1df552ae938a21f0ff3b001456ad69f7
eliascourt <elias@k1nd.ltd>
2018-04-03 10:10:11 +0100

    Added the rest of the tests for toApplicativeError

--- dog/src/test/scala/ltd/k1nd/pets/dog/syntax/BooleanOpsSpec.scala
+++ dog/src/test/scala/ltd/k1nd/pets/dog/syntax/BooleanOpsSpec.scala
@@ -5,36 +5,34 @@ import org.scalatest.{EitherValues, FlatSpec, Matchers}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import scala.util.Either
 
-class BooleanOpsSpec extends FlatSpec
-with GeneratorDrivenPropertyChecks
-with Matchers with EitherValues {
+class BooleanOpsSpec
+    extends FlatSpec
+    with GeneratorDrivenPropertyChecks
+    with Matchers
+    with EitherValues {
 
   "toApplicativeError" should "successfully infer its type arguments" in {
-    forAll{ bool: Boolean =>
+    forAll { bool: Boolean =>
       "val x: Either[Int, Unit] = bool.toApplicativeError(1337)" should compile
     }
   }
 
-  it should "turn into an context with an error on false" in {
+  it should "turn into an context with an error on false and take the other value by name" in {
     val bool = false
 
-    val leftMessage = "Ran left"
-    var state = ""
-
-    def left(): Int = {
-      state += leftMessage
-      1337
-    }
-
-    def right(): Unit = {
-      state += "Ran right"
-      ()
-    }
-
-    val result = bool.toApplicativeError(left(), right())
+    val result = bool.toApplicativeError(1337, fail())
 
     result.left.value should equal(1337)
-    state should equal(leftMessage)
+  }
+
+  it should "turn into a success on right and take the other value by name" in {
+    val bool = true
+
+    val successValue = "Great success!!"
+
+    val result = bool.toApplicativeError(fail(), successValue)
+
+    result.right.value should equal(successValue)
   }
 
 }
