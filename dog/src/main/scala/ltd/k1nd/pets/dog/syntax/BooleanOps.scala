2691e7f4f6c706f4ff931ff7da44494996c79102
eliascourt <elias@k1nd.ltd>
2018-04-03 09:58:00 +0100

    Renamed asErrorMonad to toApplicativeError

--- dog/src/main/scala/ltd/k1nd/pets/dog/syntax/BooleanOps.scala
+++ dog/src/main/scala/ltd/k1nd/pets/dog/syntax/BooleanOps.scala
@@ -11,8 +11,8 @@ trait BooleanOps {
 
 object BooleanOps {
   implicit final class BooleanSyntax(val b: Boolean) extends AnyVal {
-    def asErrorMonad[F[_]]: PartiallyAppliedErrM[F] =
-      new PartiallyAppliedErrM[F](b)
+    def toApplicativeError[F[_]]: PartiallyAppliedErrCtx[F] =
+      new PartiallyAppliedErrCtx[F](b)
     def ifElseThen[T](ifFalse: => T, ifTrue: => T): T =
       if (b) ifTrue else ifFalse
     def whenA[F[_]: Applicative, A](f: => F[A]): F[Unit] =
@@ -21,7 +21,7 @@ object BooleanOps {
       ifElseThen(l.asLeft, r.asRight)
   }
 
-  protected class PartiallyAppliedErrM[F[_]](val b: Boolean) extends AnyVal {
+  protected class PartiallyAppliedErrCtx[F[_]](val b: Boolean) extends AnyVal {
     def apply[L, R](l: => L, r: => R = ())(
         implicit appErr: ApplicativeError[F, L]): F[R] =
       new BooleanSyntax(b).ifElseThen(appErr.raiseError[R](l), appErr.pure(r))
