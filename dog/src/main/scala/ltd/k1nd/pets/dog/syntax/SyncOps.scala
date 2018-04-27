c09aeb315a545056cf3916435ebf4b40f2c22d12
eliascourt <elias@k1nd.ltd>
2018-04-27 12:13:35 +0100

    Added delay op

---
+++ dog/src/main/scala/ltd/k1nd/pets/dog/syntax/SyncOps.scala
@@ -0,0 +1,16 @@
+package ltd.k1nd.pets.dog.syntax
+
+import cats.effect.Sync
+import ltd.k1nd.pets.dog.syntax.SyncOps.SyncSyntax
+import machinist.DefaultOps
+
+trait SyncOps {
+  implicit def createSyncSyntax[T](value: T): SyncSyntax[T] =
+    new SyncSyntax[T](value)
+}
+
+object SyncOps {
+  implicit final class SyncSyntax[T](val lhs: T) extends AnyVal {
+    def delay[F[_] : Sync]: F[T] = macro DefaultOps.unopWithEv[Sync[F], F[T]]
+  }
+}
