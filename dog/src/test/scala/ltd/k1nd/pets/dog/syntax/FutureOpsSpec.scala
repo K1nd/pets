ecc87f4ff54c5d30a9e0db07cd73fc6e5e7b9eef
eliascourt <elias@k1nd.ltd>
2018-05-01 18:32:30 +0100

    Made FutureOps test slightly more complex

--- dog/src/test/scala/ltd/k1nd/pets/dog/syntax/FutureOpsSpec.scala
+++ dog/src/test/scala/ltd/k1nd/pets/dog/syntax/FutureOpsSpec.scala
@@ -8,7 +8,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class FutureOpsSpec extends FlatSpec with Matchers with MockFactory{
   "FutureOps" should "convert to IO without starting the future itself" in {
     implicit val mockCtx: ExecutionContext = mock[ExecutionContext]
-    Future(println("On the side, I'm an effect.")).toIO
+    Future(println("On the side, I'm an effect.")).map("hey" + _).toIO
     (mockCtx.execute _).expects(*).never()
   }
 }
