0eacd9f4c90001f8ef40c45c5f416cbd121fff6c
wunderk1nd-e <elias@k1nd.ltd>
2019-02-18 11:01:17 +0000

    Slight build rejigging

--- build.sbt
+++ build.sbt
@@ -21,14 +21,7 @@ lazy val dog = project
       "org.typelevel" %% "cats-core" % "1.0.1",
       "org.typelevel" %% "cats-effect" % "0.10",
       "org.typelevel" %% "machinist" % "0.6.4"
-    ),
-    addCompilerPlugin("org.spire-math" % "kind-projector" % "0.9.6" cross CrossVersion.binary),
-    libraryDependencies ++= (scalaBinaryVersion.value match {
-      case "2.10" =>
-        compilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full) :: Nil
-      case _ =>
-        Nil
-    })
+    )
   )
   .withCommonSettings
   .withReleaseSettings
--- project/ScalaSettings.scala
+++ project/ScalaSettings.scala
@@ -3,25 +3,21 @@ import sbt.Keys._
 
 object ScalaSettings extends AutoPlugin {
   object autoImport {
-    implicit class SettingsOps(val proj: Project) extends AnyVal{
+    implicit class SettingsOps(val proj: Project) extends AnyVal {
       def withCommonSettings: Project = proj.settings(
         commonSettings
       )
     }
   }
 
-  lazy val scala210 = "2.10.7"
-  lazy val scala211 = "2.11.12"
-  lazy val scala212 = "2.12.6"
-
   lazy val scalatestVersion = "3.0.5"
 
   lazy val commonSettings = Seq(
+    scalaVersion := "2.12.8",
     organization := "ltd.k1nd",
     organizationName := "KIND Consulting Ltd.",
     organizationHomepage := Some(url("https://k1nd.ltd")),
-    scalaVersion := scala212,
-    crossScalaVersions := Seq(scala210, scala211, scala212),
+    addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.9"),
     libraryDependencies ++= Seq(
       "org.scalatest" %% "scalatest" % scalatestVersion,
       "org.scalactic" %% "scalactic" % scalatestVersion,
@@ -29,14 +25,16 @@ object ScalaSettings extends AutoPlugin {
       "org.scalamock" %% "scalamock" % "4.1.0"
     ).map(_ % Test),
     scalacOptions ++= Seq(
-      "-deprecation",                      // Emit warning and location for usages of deprecated APIs.
-      "-encoding", "utf-8",                // Specify character encoding used by source files.
-      "-explaintypes",                     // Explain type errors in more detail.
-      "-feature",                          // Emit warning and location for usages of features that should be imported explicitly.
-      "-language:existentials",            // Existential types (besides wildcard types) can be written and inferred
-      "-language:experimental.macros",     // Allow macro definition (besides implementation and application)
-      "-language:higherKinds",             // Allow higher-kinded types
-      "-language:implicitConversions"     // Allow definition of implicit functions called views
+      "-Ypartial-unification",
+      "-deprecation", // Emit warning and location for usages of deprecated APIs.
+      "-encoding",
+      "utf-8", // Specify character encoding used by source files.
+      "-explaintypes", // Explain type errors in more detail.
+      "-feature", // Emit warning and location for usages of features that should be imported explicitly.
+      "-language:existentials", // Existential types (besides wildcard types) can be written and inferred
+      "-language:experimental.macros", // Allow macro definition (besides implementation and application)
+      "-language:higherKinds", // Allow higher-kinded types
+      "-language:implicitConversions" // Allow definition of implicit functions called views
     )
   )
 }
